d517e52577777eea25658c84726a12c0b8727bd2
Jose Diaz <jose@aircury.com>
2024-01-01 09:28:04 +0000

    CSV Objects | Upgraded TreeBuilder call

--- src/CSVObjects/CSVObjectsBundle/DependencyInjection/Configuration.php
+++ src/CSVObjects/CSVObjectsBundle/DependencyInjection/Configuration.php
@@ -12,8 +12,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('csv_objects');
+        $treeBuilder = new TreeBuilder('csv_objects');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNodeChildren = $rootNode->children();
 
